0a50e54940c5189ba025557d987588a70760eb79
eamargant <esteveabatx@gmail.com>
2022-11-07 12:35:24 +0100

    JS

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,28 +5,23 @@ console.log(typeof si);
 
 alert(si); 
 let person = prompt("Introduce tu nombre", "Esteve Amargant Blanco");
-alert(person);*/
+alert(person);
 var nom = "Esteve", edad = 22, home = true, dona = false, dni = null, direccio, si = Symbol("No es pot cambiar");
 const persona = {name: "Esteve", apellido: "Amargant", edad: 22, Dni: "39422511M", sexo: "A todas horas"}
-console.log(nom);
-console.log("Tipo de variable " + typeof nom);
-console.log("----------------------");
-console.log(edad);
-console.log("Tipo de variable " + typeof edad);
-console.log("----------------------");
-console.log(home);
-console.log("Tipo de variable " + typeof home);
-console.log("----------------------");
-console.log(dona);
-console.log("Tipo de variable " + typeof dona);
-console.log("----------------------");
-console.log(dni);
-console.log("Tipo de variable " + typeof dni);
-console.log("----------------------");
-console.log(direccio);
-console.log("Tipo de variable " + typeof direccio);
-console.log("----------------------");
-console.log(si.description);
-console.log("Tipo de variable " + typeof si);
-console.log("----------------------");
-console.table(persona);
+console.table(persona);*/
+var myName = "Esteve Amargant Blanco", bool = true;
+const funcio = (nom) =>{
+    return nom;
+}
+console.log(funcio(myName));
+
+function funko(numero) {
+    console.log(numero);
+}
+funko(bool);
+const saludar = function(...params) {
+    params.forEach(element => {
+        console.log(element)
+    });
+};
+saludar(1,2,3,4,5,6,7,8,9,10);
