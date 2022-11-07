7b5ee81d3dda23d2023242c3fb4e7ba223250d24
eamargant <esteveabatx@gmail.com>
2022-11-07 10:35:34 +0100

    JS

---
+++ JavaScript/main.js
@@ -0,0 +1,6 @@
+let si = "Esteve Amargant Blanco";
+console.log(si);
+console.log(typeof si);
+const persona = {name: "Esteve", apellido: "Amargant", edad: 22, Dni: "39422511M", sexo: "A todas horas"}
+console.table(persona);
+alert(si);
