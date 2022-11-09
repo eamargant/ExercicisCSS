48b7eb0e293591b58cf62b95aeb06bd935099499
eamargant <esteveabatx@gmail.com>
2022-11-09 10:58:55 +0100

    Javascript

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,69 +10,11 @@ var nom = "Esteve", edad = 22, home = true, dona = false, dni = null, direccio, 
 const persona = {name: "Esteve", apellido: "Amargant", edad: 22, Dni: "39422511M", sexo: "A todas horas"}
 console.table(persona);*/
 
-var numero = 1;
-var nom = "uno",num ="4";
-console.log(typeof num);
-switch (numero) {
-    case 1:
-        console.log("Gener");
-        break;
-    case 2:
-        console.log("Febrer");
-        break;
-    case 3:
-        console.log("Març");
-        break;
-    case 4:
-        console.log("Abril");
-        break;
-    case 5:
-        console.log("Maig");
-        break;
-    case 6:
-        console.log("Juny");
-        break;
-    case 7:
-        console.log("Juliol");
-        break;
-    case 8:
-        console.log("Agost");
-        break;
-    case 9:
-        console.log("Septembre");
-        break;
-    case 10:
-        console.log("Octubre");
-        break;
-    case 11:
-        console.log("Novembre");
-        break;
-    case 12:
-        console.log("Desembre");
-        break;
 
-    default:
-        break;
-}
+//el random de por si devuelve un numero decimal entre el 0 y el 1, asi que si queremos un rango tenemos que hacerlo manual.
+var numRandom = Math.random()* 10 +1;
 
-switch (num) {
-    case "1":
-        console.log(parseInt(num));
-        break;
-    case "2":
-        console.log(parseInt(num));
-        break;
-    case "3":
-        console.log(parseInt(num));
-        break;
-    case "4":
-        console.log(parseInt(num));
-        break;
-    case "5":
-        console.log(parseInt(num));
-        break;
-
-    default:
-        break;
-}
+console.log("Número random: "+ Math.trunc(numRandom));
+console.log("-----------------");
+console.log("Pi redondeado: "+ Math.trunc(Math.PI));
 
