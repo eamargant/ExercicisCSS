ff7f0662818f9eed7c7c73b027484a847418b6a8
eamargant <esteveabatx@gmail.com>
2022-11-09 13:50:09 +0100

    Javascript

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,14 +9,12 @@ alert(person);
 var nom = "Esteve", edad = 22, home = true, dona = false, dni = null, direccio, si = Symbol("No es pot cambiar");
 const persona = {name: "Esteve", apellido: "Amargant", edad: 22, Dni: "39422511M", sexo: "A todas horas"}
 console.table(persona);*/
-var ia = 1;
-for (let i = 1; i <= 10; i++) {
-    console.log(i + ": I ❤ code");
-    
-}
-console.log("---------------");
-while (ia <= 10) {
-    console.log(ia + ": I ❤ code");
-    ia++;
-}
+var array = ['a','b','c','d','e'];
 
+array.forEach(element => {
+    console.log(element);
+});
+console.log("--------------------");
+for (let numero = 5; numero >= 0; numero--){
+    console.log(numero);
+}
