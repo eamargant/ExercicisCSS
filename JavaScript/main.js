6c794c42183f162260f2d4ae9d18d7bdfb753175
eamargant <48244055+eamargant@users.noreply.github.com>
2022-11-09 19:54:28 +0100

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,12 +9,17 @@ alert(person);
 var nom = "Esteve", edad = 22, home = true, dona = false, dni = null, direccio, si = Symbol("No es pot cambiar");
 const persona = {name: "Esteve", apellido: "Amargant", edad: 22, Dni: "39422511M", sexo: "A todas horas"}
 console.table(persona);*/
-var array = ['a','b','c','d','e'];
-
+var array = ["🍔","🌯","🍣","🍕","🍜","🍱","🍙","🍘","🥩"];
+var posicio;
+let i =0;
+array.find(item => {
+    if (item == "🍕"){
+        posicio = array.indexOf(item);
+    }
+});
+console.log(posicio);
+array.fill("🍺",posicio);
 array.forEach(element => {
     console.log(element);
 });
-console.log("--------------------");
-for (let numero = 5; numero >= 0; numero--){
-    console.log(numero);
-}
+
