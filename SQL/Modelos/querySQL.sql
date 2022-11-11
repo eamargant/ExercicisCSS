c181f337152e09127bc951f51c2efade90b6a312
eamargant <esteveabatx@gmail.com>
2022-11-11 13:52:23 +0100

    SQL

---
+++ SQL/Modelos/querySQL.sql
@@ -0,0 +1,10 @@
+USE proba_1;
+INSERT INTO country
+VALUES(1,"Andorra","2017-07-23 13:10:11"),
+	  (2,"Afganistán","2018-05-10 12:16:56"),
+      (3,"Españita","2020-02-20 07:07:00"),
+      (4,"Portugal","2015-05-19 15:20:19"),
+      (5,"Andorra","2001-01-27 14:00:00");
+      
+SELECT * 
+FROM country;
